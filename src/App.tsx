ffe056ddd05a3be0f92781a79968087621ab3556
OnigiriKing <oooNikita87@gmail.com>
2025-11-26 11:40:14 +0900

    React lazy added

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import HeaderPage from "@/pages/Header/HeaderPage";
 import HomePage from "@/pages/Body/HomePage";
 import FooterPage from "@/pages/Footer/FooterPage";
 import { Route, Routes } from "react-router-dom";
-import SchedulePage from "@/pages/Schedule/SchedulePage";
-import ClassesPage from "@/pages/ClassesPage/ClassesPage";
-import ContactPage from "@/pages/Contact/ContactPage";
-import BlogPage from "@/pages/Blog/BlogPage";
 import UserPage from "@/pages/User/UserPage";
 import InfoPage from "@/pages/Info/InfoPage";
+import { lazy, Suspense } from "react";
+
+const SchedulePage = lazy(() => import("@/pages/Schedule/SchedulePage"));
+const BlogPage = lazy(() => import("@/pages/Blog/BlogPage"));
+const ClassesPage = lazy(() => import("@/pages/ClassesPage/ClassesPage"));
+const ContactPage = lazy(() => import("@/pages/Contact/ContactPage"));
+
 
 export default function App() {
   return (
@@ -15,13 +18,15 @@ export default function App() {
       <HeaderPage />
       <UserPage />
       <InfoPage />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/schedule" element={<SchedulePage />} />
-        <Route path="/blog" element={<BlogPage />} />
-        <Route path="/classes" element={<ClassesPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/schedule" element={<SchedulePage />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/classes" element={<ClassesPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+      </Suspense>
       <FooterPage />
     </div>
   );
